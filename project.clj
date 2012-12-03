625626b71e3874d245388226618099227cfc7c20
Nitin Punjabi <nptoronto@yahoo.com>
2012-12-03 14:01:49 +0100

    Forgot to start new branch

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject fix-translator "0.1"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [cheshire "5.0.0"]]
+  :main fix-translator.core)
--- src/fix_translator/core.clj
+++ src/fix_translator/core.clj
@@ -1,1 +1,30 @@
-(ns fix-translator.core)
+(ns fix-translator.core
+  (:require (cheshire.core :as c)))
+
+(def codecs (atom {}))
+
+(defn invert-map [m]
+  (let [new-keys (vals m)
+        new-vals (keys m)]
+    (zipmap new-keys new-vals)))
+
+(defn gen-transformations [tag-spec]
+  (let [instruction (:transform-by tag-spec)]
+    (case instruction
+      "by-value"  {:outbound #((:values tag-spec) %)
+                   :inbound  #((invert-map (:values tag-spec)) %)}
+      "to-int"    {:outbound #(str %)
+                   :inbound  #(Integer/parseInt %)}
+      "to-double" {:outbound #(str %)
+                   :inbound  #(Double/parseDouble %)}
+      "to-string" {:outbound #(identity %)
+                   :inbound  #(identity %)})))
+
+(defn gen-codec [tag-name tag-spec]
+  (let [transformer (gen-transformations tag-spec)]
+    {:encoder {tag-name [(:tag tag-spec) (:outbound transformer)]}
+     :decoder {(:tag tag-spec) [tag-name (:inbound transformer)]}}))
+
+(defn load-spec [venue])
+
+
