173248459d1d1eda9e9f0a309426f86abf5c5288
Nitin Punjabi <nptoronto@yahoo.com>
2012-12-20 17:03:32 +0100

    Update versio number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fix-translator "1.02"
+(defproject fix-translator "1.03"
   :description "A library to translate FIX messages into maps and vice versa."
   :url "https://github.com/nitinpunjabi/fix-translator"
   :dependencies [[org.clojure/clojure "1.4.0"]
